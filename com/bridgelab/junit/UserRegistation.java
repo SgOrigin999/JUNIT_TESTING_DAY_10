504f7e3ed70d910c3b5db96409be79fc66a0326d
SgOrigin999 <you@example.com>
2021-06-10 17:03:14 +0530

    Password Contain AtLeast One Special,Numeric,UpperCase Character and Minimum Size is 8

--- com/bridgelab/junit/UserRegistation.java
+++ com/bridgelab/junit/UserRegistation.java
@@ -56,7 +56,7 @@ public class UserRegistation {
 	// Passwword Validation
 	public boolean passwordValidation(String str) {
 		String password = str;
-		String regex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$";
+		String regex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
 		Pattern p = Pattern.compile(regex);
 		if (password == null) {
 			return false;
